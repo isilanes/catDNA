e11e53f6beb08d70ebe5051894edcb51122a3069
isilanes <isilanes@gmail.com>
2013-02-17 20:56:06 +0100

    run.sh: locate competing engines in external dir, not single dir for each engine

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 source run.conf
 
 cutechess-cli \
-    -fcp name=$EA cmd=$BASE/$EA/$EA \
-    -scp name=$EB cmd=$BASE/$EB/$EB \
+    -fcp name=$EA cmd=$BASE/engines/$EA \
+    -scp name=$EB cmd=$BASE/engines/$EB \
     -both tc=1+1 proto=xboard restart=on book=$BOOK \
     -games 20 -repeat -concurrency 4
