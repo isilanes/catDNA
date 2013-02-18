2f330de12862d5e951f60565279c88022b0b39db
Iñaki Silanes <isilanes@gmail.com>
2013-02-18 13:08:44 +0100

    run.sh: adapt to more recent version of cutechess + move some vars to .conf

--- run.sh
+++ run.sh
@@ -2,8 +2,8 @@
 
 source run.conf
 
-cutechess-cli \
-    -fcp name=$EA cmd=$BASE/engines/$EA \
-    -scp name=$EB cmd=$BASE/engines/$EB \
-    -both tc=1+1 proto=xboard restart=on book=$BOOK \
-    -games 20 -repeat -concurrency 4
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CUTEBASE/lib $CUTEBASE/cutechess-cli \
+    -engine name=$EA cmd=$BASE/engines/$EA \
+    -engine name=$EB cmd=$BASE/engines/$EB \
+    -each tc=1+4 proto=xboard restart=on book=$BOOK \
+    -games $ROUNDS -repeat -concurrency 4 -recover -wait 1000
