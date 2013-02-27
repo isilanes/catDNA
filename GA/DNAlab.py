e6fcd6f78e92f4480d70fe6e08a5e915d1ba6230
isilanes <isilanes@gmail.com>
2013-02-27 19:57:42 +0100

    Relocated GA stuff

---
+++ GA/DNAlab.py
@@ -0,0 +1,16 @@
+from libdna import GA
+
+#----------------------------------------------------------------------#
+
+# Initialize stuff:
+P = GA.Population(2)
+
+# Optimization loop:
+for i in range(1):
+    # Refresh population:
+    P.members[0].sequence[0] = 310
+    P.members[1].sequence[3] = 900
+
+    # Generate catDNA executable:
+    for m in P.members:
+        print(m.sequence)
--- GA/libdna/GA.py
+++ GA/libdna/GA.py
@@ -23,16 +23,3 @@ class Population:
             self.members.append(Genome())
 
 #----------------------------------------------------------------------#
-
-# Initialize stuff:
-P = Population(2)
-
-# Optimization loop:
-for i in range(1):
-    # Refresh population:
-    P.members[0].sequence[0] = 310
-    P.members[1].sequence[3] = 900
-
-    # Generate catDNA executable:
-    for m in P.members:
-        print(m.sequence)
