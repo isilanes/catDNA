0aaa686773c7c222e5a57b87e284eb2849f29698
isilanes <isilanes@gmail.com>
2013-03-01 16:24:59 +0100

    whatever

--- GA/DNAlab.py
+++ GA/DNAlab.py
@@ -7,9 +7,9 @@ P = GA.Population(4)
 
 # Initialize population:
 P.genomes[0].sequence[0] = 200
-P.genomes[1].sequence[1] = 200
-P.genomes[2].sequence[2] = 200
-P.genomes[3].sequence[3] = 200
+P.genomes[1].sequence[1] = 350
+P.genomes[2].sequence[3] = 500
+P.genomes[3].sequence[3] = 750
 
 # Optimization loop:
 for generation in range(2):
