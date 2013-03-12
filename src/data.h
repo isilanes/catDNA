4a20d34ed8e3124fa9571e588a6d1622fced51a7
isilanes <isilanes@gmail.com>
2013-03-12 19:56:56 +0100

    Merged data.h

--- src/data.h
+++ src/data.h
@@ -1,7 +1,10 @@
 /* Just a copy of data.c */
 #include <time.h>
+#include <stdio.h>
 
 MOVE bestMove;
+
+FILE *logfile;
 
 clock_t max_time;
 clock_t total_time;
@@ -40,20 +43,10 @@ int pst_queen[64];
 int pst_king_midgame[64];
 int pst_king_endgame[64];
 int flip[64];
-
 /* The king distance vars */
 int qk_dist[64][64];
 int rk_dist[64][64];
 int nk_dist[64][64];
 int bk_dist[64][64];
-
 /* Contador para la regla de los 50 movimientos */
 int fifty;
-
-/* PST-like tables */
-
-/* corr0 is corr0[piece] (value of piece) */
-//int corr0[6];
-
-/* corr1 is corr1[square][piece][color] */
-//int corr1[64][6][2];
